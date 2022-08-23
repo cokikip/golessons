4e0f21b73eec26ab4f451a19bc147a0ddb79e46e
cokikip <cokicollinji@gmail.com>
2022-08-23 12:41:06 +0300

    feat: lesson2:variable

--- src/github.com/cokikip/02_vars/main.go
+++ src/github.com/cokikip/02_vars/main.go
@@ -2,15 +2,25 @@ package main
 
 import "fmt"
 
+var name string = "Collins"
+
 func main() {
 	//Create a variable using var
 	// var with variable type
-	var name string = "Collins"
+	// var name string = "Collins"
+
+	// shorthand var
+	size := 1.9993
+	// name := "Collins"
+	name, email := "Collins", "collins@gmail.com"
+
 	//var without type
 	var age = 26
 	// using const you cannot reassign the variable
 	const isCool = true
-	fmt.Println(name, age, isCool)
+	fmt.Println(name, age, isCool, size, email)
 	fmt.Printf("%T\n", name)
 	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isCool)
+	fmt.Printf("%T\n", size)
 }
