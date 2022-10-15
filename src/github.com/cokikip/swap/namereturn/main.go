e80b24e4a0bf23f58d94e82b8c25b0f627c2cc31
cokikip <cokicollinji@gmail.com>
2022-10-15 14:41:36 +0300

    feat: return name return values

---
+++ src/github.com/cokikip/swap/namereturn/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// using naked func  -  that is Named return values
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
