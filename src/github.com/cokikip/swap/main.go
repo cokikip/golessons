10077d4acd0f538831041452091871fa8780daa6
cokikip <cokicollinji@gmail.com>
2022-10-15 14:33:20 +0300

    feat: return more than one values

--- src/github.com/cokikip/swap/main.go
+++ src/github.com/cokikip/swap/main.go
@@ -6,8 +6,14 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
-// Return more than one valuec
+func addSub(x, y int) (int, int) {
+	a := x + y
+	b := x - y
+	return a, b
+}
+
 func main() {
-	a, b := swap("hello", "world")
+	a, b := swap("Collins", "Kiplagat")
 	fmt.Println(a, b)
+	fmt.Println(addSub(9, 3))
 }
